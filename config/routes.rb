2b2e042d75e5a2abb7161555b9ec810f96981260
FengYue <10761101@gm.nfu.edu.tw>
2022-01-05 14:16:13 +0800

    upvote & downvote & about

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  root "topics#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :topics do
+    member do
+      post 'upvote'
+      post 'downvote'
+    end
+  end
+
+  get '/about/', to:'topics#about'
 end
